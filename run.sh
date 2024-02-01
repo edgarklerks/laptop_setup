5f6684c13f1d4498278e8339acec184a5c4b15f2
Edgar Klerks <edgar.klerks@gmail.com>
2024-02-01 07:30:44 +0100

    run templates under same task

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-sudo true
-ansible-playbook -e @laptop.yaml playbooks/main.yaml
+if sudo true; then 
+        ansible-playbook -e @laptop.yaml playbooks/main.yaml
+else 
+        echo "Need sudo rights" 1>&2
+fi 
