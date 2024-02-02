8d5dfef51c4796ad20787f85249bb23679ffcd98
Edgar Klerks <edgarklerks@mbp-m7j037v6th.home>
2024-02-02 18:43:06 +0100

    fixed paths

--- conf/local_bin.fish
+++ conf/local_bin.fish
@@ -2,3 +2,5 @@ fish_add_path $HOME/bin/
 fish_add_path $HOME/.local/kitty/bin/ 
 fish_add_path $HOME/.ghcup/bin/
 fish_add_path $HOME/.cabal/bin
+fish_add_path /opt/homebrew/bin
+fish_add_path $HOME/Library/Application Support/Coursier/bin
