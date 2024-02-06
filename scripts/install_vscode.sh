6e0b4baa12a938ec19fb4f62c019695eed168b07
Edgar Klerks <edgar.klerks@gmail.com>
2024-02-06 11:11:34 +0100

    added more macos scripts

---
+++ scripts/install_vscode.sh
@@ -0,0 +1,5 @@
+rm /tmp/vscode.zip
+wget "https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal" -O /tmp/vscode.zip
+cd /tmp/
+unzip vscode.zip
+mv /tmp/Visual\ Studio\ Code.app /Applications/
